ca74d7d1c2507245e99058440cb426a4c225fa83
Stefan Berger <stefanb@linux.ibm.com>
2021-11-16 07:14:50 -0500

    ima: Only accept AUDIT rules for non-init_ima_ns namespaces for now
    Only accept AUDIT rules for non-init_ima_ns namespaces for now. Reject
    all rules that require support for measuring, appraisal, and hashing.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    Acked-by: Christian Brauner <brauner@kernel.org>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>
    
    ---
    v9:
      - Jump to err_audit when unsupported rules are detected

--- security/integrity/ima/ima_policy.c
+++ security/integrity/ima/ima_policy.c
@@ -1812,6 +1812,17 @@ static int ima_parse_rule(struct ima_namespace *ns,
 			result = -EINVAL;
 			break;
 		}
+
+		/* IMA namespace only accepts AUDIT rules */
+		if (ns != &init_ima_ns && result == 0) {
+			switch (entry->action) {
+			case MEASURE:
+			case APPRAISE:
+			case HASH:
+				result = -EINVAL;
+				goto err_audit;
+			}
+		}
 	}
 	if (!result && !ima_validate_rule(entry))
 		result = -EINVAL;
@@ -1824,6 +1835,7 @@ static int ima_parse_rule(struct ima_namespace *ns,
 		check_template_modsig(template_desc);
 	}
 
+err_audit:
 	audit_log_format(ab, "res=%d", !result);
 	audit_log_end(ab);
 	return result;
