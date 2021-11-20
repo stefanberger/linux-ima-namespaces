0896ee1b61c0ee73fa88c5ea8ee5bb2a91ccf279
Stefan Berger <stefanb@linux.ibm.com>
2021-11-20 13:06:20 -0500

    ima: Use mac_admin_ns_capable() to check corresponding capability
    Use mac_admin_ns_capable() to check corresponding capability to allow
    read/write IMA policy without CAP_SYS_ADMIN but with CAP_MAC_ADMIN.
    
    Signed-off-by: Denis Semakin <denis.semakin@huawei.com>
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- security/integrity/ima/ima_fs.c
+++ security/integrity/ima/ima_fs.c
@@ -385,7 +385,8 @@ static const struct seq_operations ima_policy_seqops = {
  */
 static int ima_open_policy(struct inode *inode, struct file *filp)
 {
-	struct ima_namespace *ns = ima_ns_from_file(filp);
+	struct user_namespace *user_ns = ima_user_ns_from_file(filp);
+	struct ima_namespace *ns = user_ns->ima_ns;
 
 	if (!(filp->f_flags & O_WRONLY)) {
 #ifndef	CONFIG_IMA_READ_POLICY
@@ -393,7 +394,7 @@ static int ima_open_policy(struct inode *inode, struct file *filp)
 #else
 		if ((filp->f_flags & O_ACCMODE) != O_RDONLY)
 			return -EACCES;
-		if (!capable(CAP_SYS_ADMIN))
+		if (!mac_admin_ns_capable(user_ns))
 			return -EPERM;
 		return seq_open(filp, &ima_policy_seqops);
 #endif
