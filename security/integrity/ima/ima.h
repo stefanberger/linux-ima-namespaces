555744c2d793195a28bcc2e16935138fa7ea680e
Stefan Berger <stefanb@linux.ibm.com>
2021-12-03 18:10:22 -0500

    ima: Move dentry into ima_namespace and others onto stack
    Move the policy file dentry into the ima_namespace for reuse by
    virtualized SecurityFS and for being able to remove it from
    the filesystem. Move the other dentries onto the stack.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- security/integrity/ima/ima.h
+++ security/integrity/ima/ima.h
@@ -146,6 +146,8 @@ struct ima_namespace {
 	struct mutex ima_write_mutex;
 	unsigned long ima_fs_flags;
 	int valid_policy;
+
+	struct dentry *policy_dentry;
 } __randomize_layout;
 
 extern const int read_idmap[];
