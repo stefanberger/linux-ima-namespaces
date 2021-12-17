40696e5c38585fa65ec14862434142736be5608e
Stefan Berger <stefanb@linux.ibm.com>
2021-12-17 17:41:53 -0500

    ima: Define ima_namespace struct and start moving variables into it
    Define the ima_namespace structure and the ima_namespace variable
    init_ima_ns for the host's IMA namespace. Implement the basic functions
    ima_ns_init() and ima_init_namespace() for namespacing support.
    
    Move variables related to the IMA policy into the ima_namespace. This way
    the IMA policy of an IMA namespace can be set and displayed using a
    front-end like securityfs.
    
    In preparation for IMA namespacing, update the existing functions to
    pass the ima_namespace struct. For now, use &init_ima_ns as the current
    ima_namespace when a function that is related to a policy rule is called.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    Acked-by: Christian Brauner <brauner@kernel.org>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>
    
    ---
    
    v11:
     - Updated commit text
     - Added comments to some fields in the ima_namespace struct
    
    v9:
     - squashed patched 2 and 3 of v8
     - ima_post_read_file: only access ima_appraise in case of init_ima_ns

---
+++ security/integrity/ima/ima_init_ima_ns.c
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2016-2022 IBM Corporation
+ * Author:
+ *   Yuqiong Sun <suny@us.ibm.com>
+ *   Stefan Berger <stefanb@linux.vnet.ibm.com>
+ */
+
+#include "ima.h"
+
+static int ima_init_namespace(struct ima_namespace *ns)
+{
+	INIT_LIST_HEAD(&ns->ima_default_rules);
+	INIT_LIST_HEAD(&ns->ima_policy_rules);
+	INIT_LIST_HEAD(&ns->ima_temp_rules);
+	ns->ima_rules = (struct list_head __rcu *)(&ns->ima_default_rules);
+	ns->ima_policy_flag = 0;
+
+	return 0;
+}
+
+int __init ima_ns_init(void)
+{
+	return ima_init_namespace(&init_ima_ns);
+}
+
+struct ima_namespace init_ima_ns = {
+};
+EXPORT_SYMBOL(init_ima_ns);
