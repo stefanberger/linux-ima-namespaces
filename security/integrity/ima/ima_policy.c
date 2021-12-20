f161505c4a6fd1904d1e58ae4a9e8bf94d6d2cd7
Stefan Berger <stefanb@linux.ibm.com>
2021-12-20 10:22:45 -0500

    ima: Enable IMA namespaces
    Introduce the IMA_NS in Kconfig for IMA namespace enablement.
    
    Enable the lazy initialization of an IMA namespace when a user mounts
    SecurityFS and writes '1' into IMA's 'active' securityfs file. A
    user_namespace will now get a pointer to an ima_namespace and therefore
    implement get_current_ns() for the namespacing case that returns this
    pointer. Use get_current_ns() in those places that require access to the
    current IMA namespace. In some places, primarily those related to
    IMA-appraisal and changes to file attributes, keep the pointer to
    init_ima_ns, since there flags related to file measurements may be
    affected, which are not supported in IMA namespaces, yet.
    
    Before using the ima_namespace pointer test it with ns_is_active()
    to check whether it is NULL and whether the ima_namespace is active.
    If it's not active, it cannot be used, yet. Therefore, return early
    from those functions that may now get either get a NULL pointer from
    this call or where ns->active is still 0. The init_ima_ns is always
    set to be active, thus passing the check.
    
    Implement ima_ns_from_file() for SecurityFS-related files where we can
    now get the IMA namespace via the user namespace pointer associated
    with the superblock of the SecurityFS filesystem instance.
    
    Return -EACCES to IMA's securityfs files, except for the 'active' file,
    until the IMA namespace has been set to active.
    
    Switch access to userns->ima_ns to use acquire/release semantics to ensure
    that a newly created ima_namespace structure is fully visible upon access.
    
    Only emit the kernel log message 'policy update completed' for the
    init_ima_ns.
    
    When parsing an IMA policy rule use the user namespace of the opener
    to translate uid and gid values to kernel values rather than the user
    namespace of the writer.
    
    Gate access to ima_appraise variable to init_ima_ns in ima_load_data()
    and ima_write_policy().
    
    Gate access to temp_ima_appraise variable to init_ima_ns in
    ima_delete_rules().
    
    In ima_file_free remove the check on the ima_policy_flag so that
    ima_check_last_writer() is called if there's an iint and therefore a
    namespace is interested in the file.
    
    Only update the xattr hash if a namespace has a hash policy.
    
    Gate the setting of AUDIT rules to only allow host root with CAP_SYS_ADMIN
    inside a namespace or host root with CAP_SYS_AMDIN entering a namespace to
    set them. This prevents normal users, who created an IMA namespace and
    have CAP_SYS_ADMIN in their user namespace, from setting these rules and
    flooding the host's audit log.
    
    Automatically select IMA_CONFIG_READ when selecting IMA_NS.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    
    ---
    v12:
     - replace set_bit() by clear_bit() to clear IMA_NS_ACTIVE
    
    v11:
     - Remove check on ima_policy_flag so that any namespace modifying a
       file will cause re-auditing of a file if there's an existing iint
     - Only update the hash on a file if a namespace has a hash policy
     - Use ima_ns_from_user_ns() in ima_ns_from_file()
     - Kconfig: Select IMA_READ_POLICY when IMA_NS is selected
    
    v10:
     - dropped ima_ns_to_user_ns(); using current_user_ns() instead
     - Pass user_namespace of file opener into ima_parse_rule and propagate
       this parameter back all the way to the initial caller in the chain
     - Gate access to ima_appraise to init_ima_ns in ima_write_policy()
    
    v9:
     - ima_post_key_create_or_update: Only handle key if in init_ima_ns
     - Removed ns == NULL checks where user_namespace is now passed
     - Defer setting of user_ns->ima_ns until end of ima_fs_ns_init();
       required new ima_free_imans() and new user_ns_set_ima_ns()
     - Only emit log message 'policy update completed' for init_ima_ns
     - Introduce get_current_ns() only in this patch
     - Check for ns == &init_ima_ns in ima_load_data()

--- security/integrity/ima/ima_policy.c
+++ security/integrity/ima/ima_policy.c
@@ -487,8 +487,8 @@ int ima_lsm_policy_change(struct notifier_block *nb, unsigned long event,
 		return NOTIFY_DONE;
 
 	ns = container_of(nb, struct ima_namespace, ima_lsm_policy_notifier);
-
-	set_bit(IMA_NS_LSM_UPDATE_RULES, &ns->ima_ns_flags);
+	if (ns_is_active(ns))
+		set_bit(IMA_NS_LSM_UPDATE_RULES, &ns->ima_ns_flags);
 
 	return NOTIFY_OK;
 }
@@ -860,11 +860,12 @@ static void add_rules(struct ima_namespace *ns,
 	}
 }
 
-static int ima_parse_rule(struct ima_namespace *ns,
+static int ima_parse_rule(struct user_namespace *user_ns,
 			  char *rule, struct ima_rule_entry *entry);
 
-static int __init ima_init_arch_policy(struct ima_namespace *ns)
+static int __init ima_init_arch_policy(struct user_namespace *user_ns)
 {
+	struct ima_namespace *ns = ima_ns_from_user_ns(user_ns);
 	const char * const *arch_rules;
 	const char * const *rules;
 	int arch_entries = 0;
@@ -892,7 +893,8 @@ static int __init ima_init_arch_policy(struct ima_namespace *ns)
 		result = strscpy(rule, *rules, sizeof(rule));
 
 		INIT_LIST_HEAD(&ns->arch_policy_entry[i].list);
-		result = ima_parse_rule(ns, rule, &ns->arch_policy_entry[i]);
+		result = ima_parse_rule(user_ns, rule,
+					&ns->arch_policy_entry[i]);
 		if (result) {
 			pr_warn("Skipping unknown architecture policy rule: %s\n",
 				rule);
@@ -907,11 +909,12 @@ static int __init ima_init_arch_policy(struct ima_namespace *ns)
 
 /**
  * ima_init_policy - initialize the default measure rules.
- * @ns: IMA namespace to which the policy belongs to
+ * @user_ns: User namespace pointig to IMA namespace to which the policy belongs
  * ima_rules points to either the ima_default_rules or the new ima_policy_rules.
  */
-void __init ima_init_policy(struct ima_namespace *ns)
+void __init ima_init_policy(struct user_namespace *user_ns)
 {
+	struct ima_namespace *ns = ima_ns_from_user_ns(user_ns);
 	int build_appraise_entries, arch_entries;
 
 	/* if !ima_policy, we load NO default rules */
@@ -941,7 +944,7 @@ void __init ima_init_policy(struct ima_namespace *ns)
 	 * and custom policies, prior to other appraise rules.
 	 * (Highest priority)
 	 */
-	arch_entries = ima_init_arch_policy(ns);
+	arch_entries = ima_init_arch_policy(user_ns);
 	if (!arch_entries)
 		pr_info("No architecture policies found\n");
 	else
@@ -1392,9 +1395,21 @@ static unsigned int ima_parse_appraise_algos(char *arg)
 	return res;
 }
 
-static int ima_parse_rule(struct ima_namespace *ns,
+/*
+ * Either host root with CAP_SYS_ADMIN in current user namespace or
+ * root with CAP_SYS_ADMIN on the host entering a namespace may set
+ * audit rules inside a namespace.
+ */
+static bool may_set_audit_rule_in_ns(kuid_t uid, struct user_namespace *user_ns)
+{
+	return (uid_eq(uid, GLOBAL_ROOT_UID) &&
+		ns_capable(user_ns, CAP_SYS_ADMIN))
+		|| capable(CAP_SYS_ADMIN);
+}
+static int ima_parse_rule(struct user_namespace *user_ns,
 			  char *rule, struct ima_rule_entry *entry)
 {
+	struct ima_namespace *ns = ima_ns_from_user_ns(user_ns);
 	struct audit_buffer *ab = NULL;
 	char *from;
 	char *p;
@@ -1645,7 +1660,7 @@ static int ima_parse_rule(struct ima_namespace *ns,
 
 			result = kstrtoul(args[0].from, 10, &lnum);
 			if (!result) {
-				entry->uid = make_kuid(current_user_ns(),
+				entry->uid = make_kuid(user_ns,
 						       (uid_t) lnum);
 				if (!uid_valid(entry->uid) ||
 				    (uid_t)lnum != lnum)
@@ -1680,7 +1695,7 @@ static int ima_parse_rule(struct ima_namespace *ns,
 
 			result = kstrtoul(args[0].from, 10, &lnum);
 			if (!result) {
-				entry->gid = make_kgid(current_user_ns(),
+				entry->gid = make_kgid(user_ns,
 						       (gid_t)lnum);
 				if (!gid_valid(entry->gid) ||
 				    (((gid_t)lnum) != lnum))
@@ -1707,7 +1722,7 @@ static int ima_parse_rule(struct ima_namespace *ns,
 
 			result = kstrtoul(args[0].from, 10, &lnum);
 			if (!result) {
-				entry->fowner = make_kuid(current_user_ns(),
+				entry->fowner = make_kuid(user_ns,
 							  (uid_t)lnum);
 				if (!uid_valid(entry->fowner) ||
 				    (((uid_t)lnum) != lnum))
@@ -1733,7 +1748,7 @@ static int ima_parse_rule(struct ima_namespace *ns,
 
 			result = kstrtoul(args[0].from, 10, &lnum);
 			if (!result) {
-				entry->fgroup = make_kgid(current_user_ns(),
+				entry->fgroup = make_kgid(user_ns,
 							  (gid_t)lnum);
 				if (!gid_valid(entry->fgroup) ||
 				    (((gid_t)lnum) != lnum))
@@ -1888,6 +1903,12 @@ static int ima_parse_rule(struct ima_namespace *ns,
 			case HASH:
 				result = -EINVAL;
 				goto err_audit;
+			case AUDIT:
+				if (!may_set_audit_rule_in_ns(current_uid(),
+							      user_ns)) {
+					result = -EPERM;
+					goto err_audit;
+				}
 			}
 		}
 	}
@@ -1919,14 +1940,15 @@ err_audit:
 
 /**
  * ima_parse_add_rule - add a rule to ima_policy_rules
- * @ns: IMA namespace that has the policy
+ * @user_ns: User namespace referencing the IMA namespace that has the policy
  * @rule - ima measurement policy rule
  *
  * Avoid locking by allowing just one writer at a time in ima_write_policy()
  * Returns the length of the rule parsed, an error code on failure
  */
-ssize_t ima_parse_add_rule(struct ima_namespace *ns, char *rule)
+ssize_t ima_parse_add_rule(struct user_namespace *user_ns, char *rule)
 {
+	struct ima_namespace *ns = ima_ns_from_user_ns(user_ns);
 	static const char op[] = "update_policy";
 	char *p;
 	struct ima_rule_entry *entry;
@@ -1951,7 +1973,7 @@ ssize_t ima_parse_add_rule(struct ima_namespace *ns, char *rule)
 
 	INIT_LIST_HEAD(&entry->list);
 
-	result = ima_parse_rule(ns, p, entry);
+	result = ima_parse_rule(user_ns, p, entry);
 	if (result) {
 		ima_free_rule(entry);
 		if (ns == &init_ima_ns)
@@ -1977,7 +1999,9 @@ void ima_delete_rules(struct ima_namespace *ns)
 {
 	struct ima_rule_entry *entry, *tmp;
 
-	temp_ima_appraise = 0;
+	if (ns == &init_ima_ns)
+		temp_ima_appraise = 0;
+
 	list_for_each_entry_safe(entry, tmp, &ns->ima_temp_rules, list) {
 		list_del(&entry->list);
 		ima_free_rule(entry);
@@ -2018,7 +2042,7 @@ static const char *const mask_tokens[] = {
 
 void *ima_policy_start(struct seq_file *m, loff_t *pos)
 {
-	struct ima_namespace *ns = &init_ima_ns;
+	struct ima_namespace *ns = ima_ns_from_file(m->file);
 	loff_t l = *pos;
 	struct ima_rule_entry *entry;
 	struct list_head *ima_rules_tmp;
@@ -2039,7 +2063,7 @@ void *ima_policy_start(struct seq_file *m, loff_t *pos)
 
 void *ima_policy_next(struct seq_file *m, void *v, loff_t *pos)
 {
-	struct ima_namespace *ns = &init_ima_ns;
+	struct ima_namespace *ns = ima_ns_from_file(m->file);
 	struct ima_rule_entry *entry = v;
 
 	rcu_read_lock();
