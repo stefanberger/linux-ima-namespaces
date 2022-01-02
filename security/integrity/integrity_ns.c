35bfb61751c6e831eb0f0016e9682ea75cd35888
Stefan Berger <stefanb@linux.ibm.com>
2022-01-01 23:36:12 -0500

    integrity: Move keyring_name array into integrity_namespace
    Move the keyring_name array into integrity_namespace and initialize it
    there for the host as before. Also initialize it for the integrity
    namespaces where keyring names prefixed with "_" are always going to be
    used.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- security/integrity/integrity_ns.c
+++ security/integrity/integrity_ns.c
@@ -15,6 +15,17 @@ struct integrity_namespace init_integrity_ns = {
 	.ima_ns = &init_ima_ns,
 #endif
 	.keyring = {NULL, },
+	.keyring_name = {
+#ifndef CONFIG_INTEGRITY_TRUSTED_KEYRING
+		"_evm",
+		"_ima",
+#else
+		".evm",
+		".ima",
+#endif
+		".platform",
+		".machine",
+	},
 };
 EXPORT_SYMBOL(init_integrity_ns);
 
@@ -29,6 +40,11 @@ struct integrity_namespace *create_integrity_ns(void)
 	ns = kmem_cache_zalloc(integrityns_cachep, GFP_KERNEL);
 	if (!ns)
 		return ERR_PTR(-ENOMEM);
+
+	ns->keyring_name[INTEGRITY_KEYRING_EVM] = "_evm";
+	ns->keyring_name[INTEGRITY_KEYRING_IMA] = "_ima";
+	ns->keyring_name[INTEGRITY_KEYRING_PLATFORM] = "";
+	ns->keyring_name[INTEGRITY_KEYRING_MACHINE] = "";
 
 	return ns;
 }
