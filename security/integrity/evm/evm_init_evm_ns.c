c56e120932e718809caf60a5c1acdb97d27f0e82
Stefan Berger <stefanb@linux.ibm.com>
2022-01-10 13:22:10 -0500

    evm: Namespace EVM and handle calls from non-init EVM-ns
    Namespace EVM by creating an evm_namespace structure.
    Prevent calls from non-init EVM namespaces to use much of the EVM
    functionality and prevent access to EVM's variables that are relevant
    only for the initial EVM namespace. In the next steps these variables
    will be moved into the evm_namespace structure.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ security/integrity/evm/evm_init_evm_ns.c
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <linux/evm.h>
+
+#include "evm.h"
+
+int evm_init_namespace(struct evm_namespace *ns,
+		       struct integrity_namespace *integrity_ns)
+{
+	ns->integrity_ns = integrity_ns;
+
+	return 0;
+}
+
+int __init evm_init_ns(void)
+{
+	return evm_init_namespace(&init_evm_ns, &init_integrity_ns);
+}
+
+struct evm_namespace init_evm_ns = {
+};
