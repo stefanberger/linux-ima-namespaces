9119178858b99b50a3931acde3c0f36b4dc2d6bb
Stefan Berger <stefanb@linux.ibm.com>
2025-06-18 15:03:39 -0400

    crypto: Add ML-DSA-44/65/87 signature verification support
    Add support for ML-DSA-44/65/87 signature verification. The code has been
    derived from the following project:
    
    https://github.com/IBM/mlca/blob/main/KAT/
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ crypto/mldsa_crypto.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+#ifndef _CRYPTO_MLDSA_CRYPTO_H
+#define _CRYPTO_MLDSA_CRYPTO_H
+
+#include <linux/types.h>
+
+#define DIL_MLDSA_44_PUB_BYTES ((size_t)1312)
+#define DIL_MLDSA_65_PUB_BYTES ((size_t)1952)
+#define DIL_MLDSA_87_PUB_BYTES ((size_t)2592)
+
+#define DIL_MLDSA_MAX_SIG_SIZE 4627 /* mldsa-87 */
+
+int mldsa_verify_internal(const unsigned char *sig, size_t sbytes,
+			  const unsigned char *msg, size_t mbytes,
+			  const unsigned char *pub, size_t pbytes,
+			  const uint8_t domsep,
+			  const unsigned char *ctx, size_t cbytes);
+
+#endif
