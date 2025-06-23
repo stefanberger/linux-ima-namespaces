65e96c03e81eab3aa85bcb8069ccf66080b590ca
Stefan Berger <stefanb@linux.ibm.com>
2025-06-23 08:33:37 -0400

    crypto: Add missing shash_desc forward declaration
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- include/crypto/sha3.h
+++ include/crypto/sha3.h
@@ -26,6 +26,8 @@ struct sha3_state {
 	u8		buf[SHA3_224_BLOCK_SIZE];
 };
 
+struct shash_desc;
+
 int crypto_sha3_init(struct shash_desc *desc);
 int crypto_sha3_update(struct shash_desc *desc, const u8 *data,
 		       unsigned int len);
